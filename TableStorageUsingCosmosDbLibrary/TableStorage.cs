334b273c89f39df8e3b9cca57f4376241ccd0a42
Rina Lingad <Czarina.Lingad@valocity.co.nz>
2021-08-24 15:26:28 +1200

    Cleanup comments

--- TableStorageUsingCosmosDbLibrary/TableStorage.cs
+++ TableStorageUsingCosmosDbLibrary/TableStorage.cs
@@ -16,18 +16,6 @@ namespace TableStorageUsingCosmosDbLibrary
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            //string name = req.Query["name"];
-
-            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            //dynamic data = JsonConvert.DeserializeObject(requestBody);
-            //name = name ?? data?.name;
-
-            //string responseMessage = string.IsNullOrEmpty(name)
-            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            //return new OkObjectResult(responseMessage);
-
             log.LogInformation("Azure Cosmos Table Samples");
             BasicSamples basicSamples = new BasicSamples();
             basicSamples.RunSamples(log, context).Wait();
